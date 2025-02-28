de85d6358dc58ea25a6c380fd3eb9c30fb2d982f
enmusubi22 <ravitora.happy@gmail.com>
2025-02-28 12:43:10 +0900

    ルート作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', function () {
+    return view('users.user');
+});
+
+Route::get('user_edit', function () {
+    return view('users.user_edit');
+});

771178006847e0899ea20559efe022777a29b88d
enmusubi22 <ravitora.happy@gmail.com>
2025-03-05 02:22:46 +0900

    3/5ユーザー画面制作

---
+++ resources/views/users/user.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ユーザー</title>
+<center>
+<h1>ユーザー編集</h1>
+
+
+
+    <label for="name">ユーザー名 </label><br>
+    <input id="name" type="text" name="name" placeholder="山田太郎"><br>
+
+    <label for="status">ステータス</label><br>
+    <input id="status" type="text" name="status"><br>
+
+    <label for="email">メールアドレス</label><br>
+    <input id="email" type="email" name="email" placeholder="taro.yammada@google.com"><br>
+
+
+    <label for="password">パスワード</label><br>
+    <input id="password" name="password" ></input><br>
+    <label for="password"> </label><br>
+    <input id="password" name="password" ></input><br>
+
+    </div>
+<br>
+    <button type="submit">編集</button> <button type="submit">戻る</button>
+  </form>
+
+</div>
+</center>
---
+++ resources/views/users/user_edit.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ユーザー編集</title>
+
+
+<table class="table users">
+<style type="text/css">
+
+
+
+table, td, th {
+  padding: 1px;
+ background:rgb(6, 6, 6);
+ border: solid 1pxrgb(0, 0, 1);
+ margin-left: auto;
+    margin-right: auto;
+  
+}
+
+table {
+background-color:rgb(6, 6, 6);
+}
+
+th { padding: 10px;
+  background:rgb(136, 201, 227);
+}
+
+</style>
+<h1>ユーザー管理</h1>
+  <thead>
+    <tr>
+      <th >ID</th>
+      <th >ユーザー名</th>
+      <th >ステータス</th>
+      <th >メールアドレス</th>
+      <th >登録日</th>
+      <th >詳細</th>
+  </thead>
+
+  
+
+
+  
+ 
+ 

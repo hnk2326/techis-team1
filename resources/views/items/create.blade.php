955c5646261aa0bdaf5b4fdfa7d7c9daa537889b
hnk2326 <hhnnkk2326@gmail.com>
2025-03-18 15:16:20 +0900

    商品登録・編集機能にバリデーションの追加

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -11,37 +11,52 @@
             @csrf
             <div class="mb-3">
                 <label for="date" class="form-label">購入日</label></br>
-                <input type="date" name="date" id="date" class="form-control" required>
+                <input type="date" name="date" id="date" class="w-75 border-secondary form-control" value="{{ old('date') }}" autofocus>
+                @error('date')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="item_name" class="form-label">商品名</label></br>
-                <input type="text" name="item_name" id="item_name" class="form-control" required>
+                <input type="text" name="item_name" id="item_name" class="w-75 border-secondary form-control" value="{{ old('item_name') }}">
+                @error('item_name')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="category_id" class="form-label">カテゴリ</label></br>
-                <select name="category_id" id="category_id" class="form-select">
+                <select name="category_id" id="category_id" class="w-75 border-secondary form-select">
                     <option value=""></option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                 </select>
+                @error('category_id')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">金額</label></br>
-                <input type="text" name="price" id="price" class="form-control">
+                <input type="text" name="price" id="price" class="w-75 border-secondary form-control" value="{{ old('price') }}">
+                @error('price')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="detail" class="form-label">詳細</label></br>
-                <textarea name="detail" id="detail" class="form-control" rows="3"></textarea>
+                <textarea name="detail" id="detail" class="w-75 border-secondary form-control" rows="3">{{ old('detail') }}</textarea>
+                @error('detail')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <!-- 登録ボタン -->
-            <button type="submit" class="btn btn-primary mb-3">登録</button>
+            <button type="submit" class="btn btn-primary w-25">登録</button>
         </form>
 
     </div>
